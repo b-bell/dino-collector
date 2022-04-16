38b950198a0e5f62708fdccc9f952722d18830c9
Brian Bell <iambrianbell@gmail.com>
2022-04-16 13:50:58 -0500

    feat: Retry when bot fails to write command

--- dinocollector.py
+++ dinocollector.py
@@ -1,7 +1,9 @@
+from cmath import log
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from retry import retry
 from webdriver_manager.chrome import ChromeDriverManager
 import configparser
 import json
@@ -17,8 +19,9 @@ login_time_secs = int(config['GENERAL']['LoginTimeSecs'])
 collection_time_hrs = int(config['GENERAL']['CollectionTimeHours'])
 days = int(config['GENERAL']['Days'])
 channel_dict = json.loads(config['GENERAL']['ChannelsAndCommands'])
-page_wait_time_secs = int(config['DEVELOPER']['PageWaitTimeSecs'])
 message_box_xpath = config['DEVELOPER']['MessageBoxXPath']
+command_tries = int(config['DEVELOPER']['CommandTries'])
+command_wait_secs = int(config['DEVELOPER']['CommandWaitSecs'])
 
 # Perform conversions
 collection_time_secs = collection_time_hrs * 60 * 60
@@ -40,18 +43,22 @@ driver.execute_script(
 )
 time.sleep(login_time_secs)
 
+@retry(tries=command_tries,delay=command_wait_secs)
+def write_command(message_box_xpath, command):
+    text_element = driver.find_element(By.XPATH, message_box_xpath)
+    text_element.send_keys(command)
+    text_element.send_keys(Keys.ENTER)
+
 # Get to work
 for i in range(iterations):
     channel_list = list(channel_dict.keys())
     for channel_url in channel_list:
         driver.get(channel_url)
-        time.sleep(page_wait_time_secs)
         try:
-            text_element = driver.find_element(By.XPATH, message_box_xpath)
-            text_element.send_keys(channel_dict[channel_url])
-            text_element.send_keys(Keys.ENTER)
+            write_command(message_box_xpath, channel_dict[channel_url])
         except:
-            logging.basicConfig(filename='error.log',
+            logging.basicConfig(
+                filename='error.log',
                 encoding='utf-8',
                 level=logging.ERROR,
                 format='%(asctime)s %(message)s'
